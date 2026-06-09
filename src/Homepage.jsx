3aee98f9480d25a9890e6f12eabdddc207d19f0f
abrahamjust <162578329+abrahamjust@users.noreply.github.com>
2026-06-09 09:05:40 +0000

    Completed Homepage design
    completed homepage css designing. Moving on to the other pages.

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -5,12 +5,14 @@ export default Homepage;
 
 function Homepage() {
     return (
-        <div className={styles.homePage}>
-            <div>
-                <h1>Trolley</h1>
-                <p>For your shopping needs</p>
+        <div className={styles.container}>
+            <div className={styles.homePage}>
+                <div>
+                    <h1 className={styles.homePageTitle}>Trolley</h1>
+                    <p className={styles.homePagePara}>For your shopping needs</p>
+                </div>
+                <Link to="/Shop" className={styles.homePageButton}>Let's Shop</Link>
             </div>
-            <Link to="/Shop">Let's Shop</Link>
         </div>
     )
 }
